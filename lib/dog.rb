e2ea27b7aea19618ac11ed2594b3cde9fda88e16
Leslie Osorio <lesosorio17@gmail.com>
2019-03-07 23:48:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,12 +4,8 @@ class Dog
   attr_accessor :id, :name, :breed
 
   def initialize(attributes)
-    
     attributes.each {|key, value| self.send(("#{key}="), value)}
     self.id ||= nil
-    #id = id
-    #name = name
-    #breed = breed
   end
 
 
