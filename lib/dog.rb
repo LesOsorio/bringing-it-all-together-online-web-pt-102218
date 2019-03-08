792067847652593c86fef21ccd822cbcae0138d3
Leslie Osorio <lesosorio17@gmail.com>
2019-03-08 01:02:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -89,9 +89,9 @@ class Dog
 
   def update
     sql = <<-SQL
-      UPDATE dogs SET name = ?, breed = ? WHERE name = ?
+      UPDATE dogs SET name = ?, breed = ? WHERE id = ?
     SQL
 
-    DB[:conn].execute(sql, self.name, self.breed)
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 end
