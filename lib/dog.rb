0f84587b1b842c29bf77258978660e32d8b74c0c
Leslie Osorio <lesosorio17@gmail.com>
2019-03-08 00:59:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -76,7 +76,7 @@ class Dog
       self.new(attributes_hash)
   end
 
-  def self.find_by_name(row)
+  def self.find_by_name(name)
     sql = <<-SQL
       SELECT * FROM dogs
       WHERE name = ?
@@ -85,7 +85,7 @@ class Dog
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end
-  end 
+  end
 
 
 end
