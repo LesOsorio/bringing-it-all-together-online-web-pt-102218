4ebe0997197a2056fbaece99010e14351e1e4b35
Leslie Osorio <lesosorio17@gmail.com>
2019-03-08 00:48:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -58,8 +58,8 @@ class Dog
       AND breed = ?
       SQL
 
-    dog = DB[:conn].execute(sql, name, breed).first
-    
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+
     if !dog.empty?
       dog_data = dog[0]
       dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
