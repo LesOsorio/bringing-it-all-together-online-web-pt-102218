3726bd5ec1bc7bc7ffa697f8c869bbc4e55a205a
Leslie Osorio <lesosorio17@gmail.com>
2019-03-08 00:22:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -41,6 +41,14 @@ class Dog
     dog
   end
 
+  def self.new_from_db(row)
+    attributes_hash = {
+      :id => row[0],
+      :name => row[1],
+      :breed => row[2]
+    }
+      self.new(attributes_hash)
+
   def self.find_by_id(id)
     sql = <<-SQL
     SELECT * FROM dogs WHERE id = ?
